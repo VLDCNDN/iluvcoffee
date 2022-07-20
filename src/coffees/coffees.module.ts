0ada865ff7aadc954d64623082617a8973cdac91
vldcndn <vldcndn@gmail.com>
2022-07-20 11:32:29 +0800

    use Class Provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,11 +7,21 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['Nescafe', 'Barako'] },
   ],
   exports: [CoffeesService],
